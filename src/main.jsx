7c75ecdb149778d2827dd7079eda7bdfe16dac83
Luciano Labudia <labudia.luciano@gmail.com>
2023-04-06 10:41:04 -0300

    Crear una pantalla de Error

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { NewClient, action as actionNewClient } from './pages/NewClient'
 import { Index, loader as loaderClients } from './pages/Index'
+import ErrorPage from './components/ErrorPage'
 
 
 const router = createBrowserRouter([
@@ -15,7 +16,8 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Index />,
-        loader: loaderClients
+        loader: loaderClients,
+        errorElement: <ErrorPage />
       },
       {
         path: '/clientes/nuevo',
